f09ae4d277df0e4178e252da148d3bf28bf61eb2
Piyush Parmar <2822piyushparmar@gmail.com>
2022-07-13 00:43:18 +0530

    Updated Image

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var categories= [
     {
         "id": "1",
         "category": "Fashion",
-        "image": "https://images.unsplash.com/photo-1548666986-81912b883425?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fGZhc2hpb24lMjAxKjF8ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
+        "image": "https://images.unsplash.com/photo-1446214814726-e6074845b4ce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGZhc2hpb258ZW58MHwyfDB8fA%3D%3D&auto=format&fit=crop&w=400&q=60"
     },
 
     {
